7fb4a9a5c66d43f13ebadd30f5386b6f449cd028
ryukyunghye <ryukyung123@naver.com>
2022-04-13 21:30:53 +0900

    java: part1

---
+++ start_java/part1.java
@@ -0,0 +1,12 @@
+// main 메소드
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
+// public : 메소드 접근제어자, 누구나 이 메소드에 접근할 수 있다
+// static : 이 메소드는 인스턴스 생성없이 실행할 수 있음
+// void : 메소드의 리턴값이 없음_텅 빈 거
+// String[] : 문자열, []_1개가 아닌 여러 개의 값으로 이루어진 배열임
+// args : String[] 자료형에 대한 변수명
+// System.out.println : 표준 출력, 자바의 내장 메소드로 문자열 화면에 출력
