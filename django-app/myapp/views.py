9cbd5d90e9c4b3b3a501953f87acf07581494449
ryukyunghye <ryukyung123@naver.com>
2022-06-27 19:27:27 +0900

    Add(folder): django7

--- django-app/myapp/views.py
+++ django-app/myapp/views.py
@@ -1,15 +1,54 @@
 from django.shortcuts import render, HttpResponse
+topics = [
+    {'id': 1, 'title': 'rounting', 'body': 'Routing is ..'},
+    {'id': 2, 'title': 'view', 'body': 'View is ..'},
+    {'id': 3, 'title': 'Model', 'body': 'Model is ..'}
+]
 
-# Create your views here.
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}
+        <ul>
+            <li><a href="/create/">create</a></li>
+        </ul>
+    </body>
+    </html>
+    '''
 
 
 def index(request):
-    return HttpResponse('Welcome!')
+    article = '''
+        <h2>Welcome</h2>
+        Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 
 def create(request):
-    return HttpResponse('Create')
+    article = '''
+        <form action = "/create/" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit"></p>
+        </form>
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 
 def read(request, id):
-    return HttpResponse('Read!'+id)
+    global topics
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
