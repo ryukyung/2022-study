249e7371e26b9fb939ff005df244b3a575cae741
ryukyunghye <ryukyung123@naver.com>
2022-07-14 02:05:20 +0900

    Add(folder): blog flask[not img]

---
+++ flask/Flask/blog/views.py
@@ -0,0 +1,66 @@
+# # blog/views.py
+# from flask import Blueprint, render_template
+
+# views = Blueprint("views", __name__)
+
+# @views.route("/")
+# def home():
+#     return render_template("index.html")
+
+# @views.route("/about")
+# def about():
+#     return render_template("about.html")
+
+
+# @views.route("/categories-list")
+# def categories_list():
+#     return render_template("categories_list.html")
+
+# @views.route("/post-list")
+# def post_list():
+#     return render_template("post_list.html")
+
+# @views.route("/login")
+# def login():
+#     return render_template("login.html")
+# @views.route("/sign-up")
+# def sign_up():
+#     return render_template("signup.html")
+
+# @views.route("posts/<int:id>")
+# def post_detail():
+#     return render_template("post_detail.html")
+
+
+# @views.route("/contact")
+# def contact():
+#     return render_template("contact.html")
+
+
+from flask import Blueprint, render_template
+
+views = Blueprint("views", __name__)
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+@views.route("/about")
+def about():
+    return render_template("about.html")
+
+@views.route("/categories-list")
+def categories_list():
+    return render_template("categories_list.html")
+
+@views.route("/post-list")
+def post_list():
+    return render_template("post_list.html")
+
+@views.route('posts/<int:id>')
+def post_detail():
+    return render_template("post_detail.html")
+
+@views.route("/contact")
+def contact():
+    return render_template("contact.html")
