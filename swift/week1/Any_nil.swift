d12a92be38ee9e77e51e6d8d43576bb16830c4ec
ryukyunghye <ryukyung123@naver.com>
2022-07-17 21:08:50 +0900

    Edit(repo)

---
+++ swift/week1/Any_nil.swift
@@ -0,0 +1,15 @@
+import Swift
+// Any
+var someAny: Any = 100
+someAny = "어떤 타입이든 받음"
+someAny = 123.123
+//let someBouble: Double = someAny -> 컴파일 오류 : Any가 Double 타입이 아니라서 할당 불가능
+
+// AnyObejct
+class someClass {}
+var someAnyObject: AnyObject = someClass()
+//someAnyObject = 123.12 -> 컴파일 오류 : 클래스의 인스턴스만 수용 가능, 클래스 인스턴스 아니면 할당 불가
+
+// nil
+//someAny = nil // 컴파일 오류
+//someAnyObject = nil // 컴파일 오류
